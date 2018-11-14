63ed3baaba50d63f12f84e8fef1bad04d683d01a
matthewguillory65 <matthewguillory65@gmail.com>
2018-11-14 10:50:00 -0600

    Making the repo

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    public List<Inventory> items;
+
+	// Use this for initialization
+	void Start ()
+    {
+        items = new List<Inventory>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Key"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
